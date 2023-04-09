8ed5ddf2065a1787693b32b334adbc7bba8ad1d4
soubj <sobujahmed124@gmail.com>
2023-04-09 23:37:01 +0600

    sending data through props

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Device from './component/Device/Device'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-   
+      <Device name="samsung" price="10000"></Device>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
--- src/component/Device/Device.jsx
+++ src/component/Device/Device.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const Device = () => {
+const Device = (props) => {
     return (
         <div>
-            
+            <h2>I have a device : {props.name}</h2>
+            <h2>The price is : {props.price}</h2>
         </div>
     );
 };
